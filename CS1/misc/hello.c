c7e8a5bcf0717726b4cdc2332ee823473ff36931
Munazza <munazzajaved277@gmail.com>
2023-10-18 17:53:38 +0500

    Made hello.c file in misc

---
+++ CS1/misc/hello.c
@@ -0,0 +1,16 @@
+/**
+     * Author: Munazza JAved
+     * Date: 2023/10/18
+     *
+     * A simple hello world program in C
+     *
+     */
+    #include <stdlib.h>
+    #include <stdio.h>
+
+    int main(int argc, char **argv) {
+
+      printf("Hello World!\n");
+
+      return 0;
+    }
